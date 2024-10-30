a7fb8cf68022fa43a16f702c9ac0da6d6dbffdcf
Milind Waykole <mwaykole@mwaykole-thinkpadp1gen4i.bengluru.csb>
2024-10-30 15:26:29 +0530

    [WIP] fix isvc for all kind of storage
    Signed-off-by: Milind Waykole <mwaykole@mwaykole-thinkpadp1gen4i.bengluru.csb>

--- tests/model_serving/model_server/storage/pvc/utils.py
+++ tests/model_serving/model_server/storage/pvc/utils.py
@@ -1,40 +1,84 @@
 from contextlib import contextmanager
+from typing import Any, Dict, Optional
 
 from kubernetes.dynamic import DynamicClient
 from ocp_resources.inference_service import InferenceService
 
-
 @contextmanager
 def create_isvc(
-    client: DynamicClient,
-    name: str,
-    namespace: str,
-    deployment_mode: str,
-    storage_uri: str,
-    model_format: str,
-    runtime: str,
-    min_replicas: int = 1,
-    wait: bool = True,
+        client: DynamicClient,
+        name: str,
+        namespace: str,
+        deployment_mode: str,
+        storage_key: str,
+        storage_uri: str,
+        model_format: str,
+        runtime: str,
+        max_replicas: Optional[int] = 1,
+        min_replicas: Optional[int] = 1,
+        cpu_limit: str = "2",
+        memory_limit: str = "8Gi",
+        cpu_request: str = "1",
+        memory_request: str = "4Gi",
+        wait: bool = True,
+        enable_auth: bool = False,
+        model_service_account: Optional[str] = ""
 ) -> InferenceService:
-    with InferenceService(
-        client=client,
-        name=name,
-        namespace=namespace,
-        annotations={
+
+
+    predictor_config: Dict[str, Any] = {
+        "model": {
+            "modelFormat": {"name": model_format},
+            "version": "1",
+            "runtime": runtime,
+            "storage": {
+                "key": storage_key,
+                "path": storage_uri
+            },
+            "resources": {
+                "limits": {
+                    "cpu": cpu_limit,
+                    "memory": memory_limit
+                },
+                "requests": {
+                    "cpu": cpu_request,
+                    "memory": memory_request
+                }
+            }
+        },
+        "maxReplicas": max_replicas,
+        "minReplicas": min_replicas
+    }
+
+    if model_service_account:
+        predictor_config["serviceAccountName"] = model_service_account
+
+    if deployment_mode == "RawDeployment":
+        annotations = {
+            "serving.kserve.io/deploymentMode": deployment_mode,
+        }
+        labels = {
+            "networking.kserve.io/visibility": "enable-route",
+            "security.opendatahub.io/enable-auth": "true",
+        }
+    else:
+        annotations = {
             "serving.knative.openshift.io/enablePassthrough": "true",
             "sidecar.istio.io/inject": "true",
             "sidecar.istio.io/rewriteAppHTTPProbers": "true",
             "serving.kserve.io/deploymentMode": deployment_mode,
-        },
-        predictor={
-            "minReplicas": min_replicas,
-            "model": {
-                "modelFormat": {"name": model_format},
-                "version": "1",
-                "runtime": runtime,
-                "storageUri": storage_uri,
-            },
-        },
+        }
+
+    if enable_auth and deployment_mode == "Serverless":
+        annotations["security.opendatahub.io/enable-auth"] = "true"
+
+    with InferenceService(
+            client=client,
+            name=name,
+            namespace=namespace,
+            label=labels,
+            annotations=annotations,
+            predictor=predictor_config,
     ) as inference_service:
         if wait:
             inference_service.wait_for_condition(
