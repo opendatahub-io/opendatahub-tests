6f103c7444ebda9735e9c20fb103fd7402a230da
rnetser <rnetser@redhat.com>
2024-12-18 15:27:39 +0200

    comments

--- .github/workflows/scripts/size_labeler.py
+++ .github/workflows/scripts/size_labeler.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from typing import Dict, Set, Tuple
 from github import Github
 from github.PullRequest import PullRequest
 from github.Repository import Repository
@@ -8,7 +7,7 @@ from simple_logger.logger import get_logger
 
 LOGGER = get_logger(name=__name__)
 
-SIZE_LABELS: Dict[Tuple[int, int], str] = {
+SIZE_LABELS: dict[tuple[int, int], str] = {
     (0, 20): "size/xs",
     (21, 50): "size/s",
     (51, 100): "size/m",
@@ -16,7 +15,7 @@ SIZE_LABELS: Dict[Tuple[int, int], str] = {
     (301, sys.maxsize): "size/xl",
 }
 
-EXCLUDED_FILES: Set[str] = {".lock", ".md"}
+EXCLUDED_FILES: set[str] = {".lock", ".md"}
 
 
 def get_pr_size(pr: PullRequest) -> int:
@@ -41,8 +40,8 @@ def main() -> None:
     repo_name: str = os.environ["GITHUB_REPOSITORY"]
     pr_number: int = int(os.environ["GITHUB_PR_NUMBER"])
 
-    g: Github = Github(github_token)
-    repo: Repository = g.get_repo(repo_name)
+    gh_client: Github = Github(github_token)
+    repo: Repository = gh_client.get_repo(repo_name)
     pr: PullRequest = repo.get_pull(pr_number)
 
     for label in pr.labels:
