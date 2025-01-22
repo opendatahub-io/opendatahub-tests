839c940a9b16fc4fa71ccae0454ef0d778609324
lugi0 <lgiorgi@redhat.com>
2025-01-22 18:01:16 +0100

    Switch to mr python client
    Signed-off-by: lugi0 <lgiorgi@redhat.com>

--- tests/model_registry/test_model_registry_creation.py
+++ tests/model_registry/test_model_registry_creation.py
@@ -1,11 +1,9 @@
 import pytest
-import shlex
-from ast import literal_eval
 from typing import Self
 from simple_logger.logger import get_logger
-from pyhelper_utils.shell import run_command
 
-from tests.model_registry.utils import generate_register_model_command
+from utilities.constants import Protocols
+from model_registry import ModelRegistry
 
 LOGGER = get_logger(name=__name__)
 
@@ -17,23 +15,49 @@ class TestModelRegistryCreation:
 
     @pytest.mark.smoke
     def test_registering_model(self: Self, model_registry_instance_rest_endpoint: str, current_client_token: str):
-        cmd = generate_register_model_command(
-            endpoint=model_registry_instance_rest_endpoint, token=current_client_token
+        # address and port need to be split in the client instantiation
+        server = model_registry_instance_rest_endpoint.split(":")[0]
+        port = model_registry_instance_rest_endpoint.split(":")[1]
+        registry = ModelRegistry(
+            server_address=f"{Protocols.HTTPS}://{server}",
+            port=port,
+            author="opendatahub-test",
+            user_token=current_client_token,
+            is_secure=False,
         )
-        _, out, _ = run_command(command=shlex.split(cmd))
-        out_dict = literal_eval(node_or_string=out)
+        model = registry.register_model(
+            name="my-model",  # model name
+            uri="https://storage-place.my-company.com",  # model URI
+            version="2.0.0",
+            description="lorem ipsum",
+            model_format_name="onnx",
+            model_format_version="1",
+            storage_key="my-data-connection",
+            storage_path="path/to/model",
+            metadata={
+                # can be one of the following types
+                "int_key": 1,
+                "bool_key": False,
+                "float_key": 3.14,
+                "str_key": "str_value",
+            },
+        )
+        registered_model = registry.get_registered_model("my-model")
         errors = []
-        if not out_dict["name"] == "model-name":
-            errors.append(f"Unexpected name, received {out_dict['name']}")
-        if not out_dict["description"] == "test-model":
-            errors.append(f"Unexpected description, received {out_dict['description']}")
-        if not out_dict["externalId"] == "1":
-            errors.append(f"Unexpected id, received {out_dict['externalId']}")
-        if not out_dict["owner"] == "opendatahub-tests-client":
-            errors.append(f"Unexpected owner, received {out_dict['owner']}")
-        if not out_dict["state"] == "LIVE":
-            errors.append(f"Unexpected state, received {out_dict['state']}")
+        if not registered_model.id == model.id:
+            errors.append(f"Unexpected id, received {registered_model.id}")
+        if not registered_model.name == model.name:
+            errors.append(f"Unexpected name, received {registered_model.name}")
+        if not registered_model.description == model.description:
+            errors.append(f"Unexpected description, received {registered_model.description}")
+        if not registered_model.owner == model.owner:
+            errors.append(f"Unexpected owner, received {registered_model.owner}")
+        if not registered_model.state == model.state:
+            errors.append(f"Unexpected state, received {registered_model.state}")
 
         assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
-    # TODO: Query for a registered Model
+    # TODO: Edit a registered model
+    # TODO: Add additional versions for a model
+    # TODO: List all available models
+    # TODO: List all versions of a model
