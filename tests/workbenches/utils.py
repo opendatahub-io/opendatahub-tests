60c1c2725f7f63856e342182770dd7dfcbcffc44
Jiri Daněk <jdanek@redhat.com>
2025-02-24 13:31:40 +0100

    fixup, dyn_client

--- tests/workbenches/utils.py
+++ tests/workbenches/utils.py
@@ -28,17 +28,17 @@ def get_notebook_image(image_name: str, image_tag: str) -> str:
     return f"{INTERNAL_IMAGE_REGISTRY_PATH}/{controllers_namespace}/{image_dict[image_name]}:{image_tag}"
 
 
-def load_default_notebook(client: DynamicClient, namespace: str, name: str, image: str) -> Notebook:
+def load_default_notebook(dyn_client: DynamicClient, namespace: str, name: str, image: str) -> Notebook:
     notebook_string = (pathlib.Path(__file__).parent / "notebook-controller/test_data/notebook.yaml").read_text()
     notebook_string = notebook_string.replace("my-project", namespace).replace("my-workbench", name)
     # Set new Route url
     route_name = "odh-dashboard" if py_config.get("distribution") == "upstream" else "rhods-dashboard"
-    route_host: str = list(Route.get(client=client, name=route_name, namespace=py_config["applications_namespace"]))[
-        0
-    ].host
+    route_host: str = list(
+        Route.get(dyn_client=dyn_client, name=route_name, namespace=py_config["applications_namespace"])
+    )[0].host
     notebook_string = notebook_string.replace("odh_dashboard_route", "https://" + route_host)
     # Set the correct username
-    username = get_username(client=client)
+    username = get_username(dyn_client=dyn_client)
     notebook_string = notebook_string.replace("odh_user", username)
     # Replace image
     notebook_string = notebook_string.replace("notebook_image_placeholder", image)
@@ -46,12 +46,12 @@ def load_default_notebook(client: DynamicClient, namespace: str, name: str, imag
     return Notebook(yaml_file=io.StringIO(notebook_string))
 
 
-def get_username(client: DynamicClient) -> str:
+def get_username(dyn_client: DynamicClient) -> str:
     """Gets the username for the client (see kubectl -v8 auth whoami)"""
-    self_subject_review_resource: Resource = client.resources.get(
+    self_subject_review_resource: Resource = dyn_client.resources.get(
         api_version="authentication.k8s.io/v1", kind="SelfSubjectReview"
     )
-    self_subject_review: ResourceInstance = client.create(self_subject_review_resource)
+    self_subject_review: ResourceInstance = dyn_client.create(self_subject_review_resource)
     username: str = self_subject_review.status.userInfo.username
     return username
 
