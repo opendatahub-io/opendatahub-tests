b0b4902fb01d4657302b5a8229c0833b755aeb9b
Jiri Daněk <jdanek@redhat.com>
2025-03-17 21:54:57 +0100

    fixup, this should either be a fixture (as setup for this test) or a separate test; each test should do one thing.

--- tests/workbenches/notebook-controller/test_spawning.py
+++ tests/workbenches/notebook-controller/test_spawning.py
@@ -1,42 +1,32 @@
 from __future__ import annotations
 
 import pytest
-from pytest_testconfig import config as py_config
 
 from ocp_resources.pod import Pod
-
-from utilities.constants import INTERNAL_IMAGE_REGISTRY_PATH
-from tests.workbenches.utils import load_default_notebook
 
 
 class TestNotebook:
     @pytest.mark.parametrize(
-        "users_namespace, users_persistent_volume_claim",
+        "users_namespace,users_persistent_volume_claim,default_notebook",
         [
             pytest.param(
                 {"name": "test-odh-notebook"},
                 {"name": "test-odh-notebook"},
+                {
+                    "namespace": "test-odh-notebook",
+                    "name": "test-odh-notebook",
+                },
             )
         ],
         indirect=True,
     )
     def test_create_simple_notebook(
-        self, admin_client, unprivileged_client, users_namespace, users_persistent_volume_claim
+        self, admin_client, unprivileged_client, users_namespace, users_persistent_volume_claim, default_notebook
     ):
         """
         Create simple Notebook with all needed resources and see if Operator creates it properly
         """
-        image_name = (
-            "jupyter-minimal-notebook" if py_config.get("distribution") == "upstream" else "s2i-minimal-notebook"
-        )
-        notebook_image: str = (
-            f"{INTERNAL_IMAGE_REGISTRY_PATH}/{py_config['applications_namespace']}/{image_name}:{'2024.2'}"
-        )
-        notebook = load_default_notebook(
-            dyn_client=admin_client, namespace=users_namespace.name, name=users_namespace.name, image=notebook_image
-        )
-
-        with notebook:
+        with default_notebook:
             pods = Pod.get(
                 dyn_client=unprivileged_client,
                 namespace=users_namespace.name,
