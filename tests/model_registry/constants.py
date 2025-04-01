d6c908145446dc7f25669856e1e3f92bd54ddd07
dbasunag <dbasunag@redhat.com>
2025-03-31 21:12:59 -0400

    Add new tests for registriesNamespace validation in datascience cluster
    updates! bb07e85
    
    updates! 37caaf4
    
    updates! 60a33aa
    
    updates! 4cfeabf

--- tests/model_registry/constants.py
+++ tests/model_registry/constants.py
@@ -1,2 +1,34 @@
+from typing import Any
+
+from utilities.constants import ModelFormat
+
+
 class ModelRegistryEndpoints:
     REGISTERED_MODELS: str = "/api/model_registry/v1alpha3/registered_models"
+
+
+MR_NAMESPACE: str = "rhoai-model-registries"
+MR_OPERATOR_NAME: str = "model-registry-operator"
+MODEL_NAME: str = "my-model"
+MODEL_DICT: dict[str, Any] = {
+    "model_name": MODEL_NAME,
+    "model_uri": "https://storage-place.my-company.com",
+    "model_version": "2.0.0",
+    "model_description": "lorem ipsum",
+    "model_format": ModelFormat.ONNX,
+    "model_format_version": "1",
+    "model_storage_key": "my-data-connection",
+    "model_storage_path": "path/to/model",
+    "model_metadata": {
+        "int_key": 1,
+        "bool_key": False,
+        "float_key": 3.14,
+        "str_key": "str_value",
+    },
+}
+MR_INSTANCE_NAME: str = "model-registry"
+ISTIO_CONFIG_DICT: dict[str, Any] = {
+    "authProvider": "redhat-ods-applications-auth-provider",
+    "gateway": {"grpc": {"tls": {}}, "rest": {"tls": {}}},
+}
+DB_RESOURCES_NAME: str = "model-registry-db"
