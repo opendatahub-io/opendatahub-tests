77ad3c313b3bb6b875fab30b0fb3537bc78c07b7
lugi0 <lgiorgi@redhat.com>
2025-04-09 17:03:20 +0200

    fix: Remove sanity mark from stateless test, readd skip mark
    Signed-off-by: lugi0 <lgiorgi@redhat.com>

--- tests/model_registry/test_rest_api.py
+++ tests/model_registry/test_rest_api.py
@@ -7,10 +7,10 @@ LOGGER = get_logger(name=__name__)
 schema = schemathesis.pytest.from_fixture("generated_schema")
 
 
+@pytest.mark.skip(reason="Only run manually for now")
 @pytest.mark.fuzzer
-@pytest.mark.sanity
 @schema.parametrize()
-def test_mr_api(case, current_client_token):
+def test_mr_api_stateless(case, current_client_token):
     case.headers["Authorization"] = f"Bearer {current_client_token}"
     case.headers["Content-Type"] = "application/json"
     case.call_and_validate(verify=False)
