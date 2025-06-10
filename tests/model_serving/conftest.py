330e4c953d92c62679dead9a2aea68164cff1a41
Edward Quarm <equarmjnr@gmail.com>
2025-06-10 17:22:29 -0400

    feat(model-validation): fix flake8 errors

--- tests/model_serving/conftest.py
+++ tests/model_serving/conftest.py
@@ -3,7 +3,7 @@ from _pytest.fixtures import FixtureRequest
 
 
 @pytest.fixture(scope="session")
-def s3_models_storage_uri(request: FixtureRequest, models_s3_bucket_name: str) -> str:
+def s3_models_storage_uri(request: FixtureRequest, models_s3_bucket_name: str) -> str | None:
     if not models_s3_bucket_name:
         return None
     return f"s3://{models_s3_bucket_name}/{request.param['model-dir']}/"
