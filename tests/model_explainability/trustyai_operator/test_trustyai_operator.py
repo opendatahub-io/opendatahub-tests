b5373c4a322a023df337013403bf43471937b7cc
Shelton Cyril <sheltoncyril@gmail.com>
2025-06-12 09:34:59 +0100

    feat: add fixture for trustyai operator deployment

--- tests/model_explainability/trustyai_operator/test_trustyai_operator.py
+++ tests/model_explainability/trustyai_operator/test_trustyai_operator.py
@@ -1,5 +1,7 @@
 import pytest
 from ocp_resources.config_map import ConfigMap
+from ocp_resources.deployment import Deployment
+
 from tests.model_explainability.trustyai_operator.utils import validate_trustyai_operator_image
 
 
@@ -8,9 +10,10 @@ def test_validate_trustyai_operator_image(
     admin_client,
     related_images_refs: set[str],
     trustyai_operator_configmap: ConfigMap,
+    trustyai_operator_deployment: Deployment,
 ):
     return validate_trustyai_operator_image(
-        client=admin_client,
         related_images_refs=related_images_refs,
         tai_operator_configmap_data=trustyai_operator_configmap.instance.data,
+        tai_operator_deployment=trustyai_operator_deployment,
     )
--- tests/model_explainability/trustyai_operator/utils.py
+++ tests/model_explainability/trustyai_operator/utils.py
@@ -1,13 +1,10 @@
-from kubernetes.dynamic import DynamicClient
 from ocp_resources.deployment import Deployment
 
-from tests.model_explainability.trustyai_service.trustyai_service_utils import TRUSTYAI_SERVICE_NAME
-from pytest_testconfig import config as py_config
 from utilities.general import validate_image_format
 
 
 def validate_trustyai_operator_image(
-    client: DynamicClient, related_images_refs: set[str], tai_operator_configmap_data: dict[str, str]
+    related_images_refs: set[str], tai_operator_configmap_data: dict[str, str], tai_operator_deployment: Deployment
 ) -> None:
     """Validates the TrustyAI operator image.
     Checks if:
@@ -15,12 +12,6 @@ def validate_trustyai_operator_image(
         - image is present in relatedImages of CSV.
         - image complies with OpenShift AI requirements i.e. sourced from registry.redhat.io and pinned w/o tags.
     """
-    tai_operator_deployment = Deployment(
-        client=client,
-        name=f"{TRUSTYAI_SERVICE_NAME}-operator-controller-manager",
-        namespace=py_config["applications_namespace"],
-        wait_for_resource=True,
-    )
     tai_operator_image = tai_operator_deployment.instance.spec.template.spec.containers[0].image
     assert tai_operator_image == tai_operator_configmap_data["trustyaiOperatorImage"]
     assert tai_operator_image in related_images_refs
