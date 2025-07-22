0a989ee173d0d8b0067a845b7184904ca2e60ecf
Edward Quarm <equarmjnr@gmail.com>
2025-07-22 09:23:18 -0400

    feat(model-validation): revert s3 changes

--- tests/model_serving/conftest.py
+++ tests/model_serving/conftest.py
@@ -37,9 +37,7 @@ def protocol(request: FixtureRequest) -> str:
 
 
 @pytest.fixture(scope="session")
-def s3_models_storage_uri(request: FixtureRequest, models_s3_bucket_name: str) -> str | None:
-    if not models_s3_bucket_name:
-        return None
+def s3_models_storage_uri(request: FixtureRequest, models_s3_bucket_name: str) -> str:
     return f"s3://{models_s3_bucket_name}/{request.param['model-dir']}/"
 
 
