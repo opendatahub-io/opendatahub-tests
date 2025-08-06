2bd61e20d863f11b89b1d85d155d94bd312f5aee
aaguirre <aaguirre@redhat.com>
2025-08-06 11:59:58 +0200

    add assertions

--- tests/model_explainability/lm_eval/test_llamastack_lmeval_provider.py
+++ tests/model_explainability/lm_eval/test_llamastack_lmeval_provider.py
@@ -32,7 +32,8 @@ class TestLlamaStackLMEvalProvider:
     def test_lmeval_register_benchmark(self, llamastack_client):
         llamastack_client.models.register(provider_id="vllm-inference", model_type="llm", model_id=MNT_MODELS)
 
-        trustyai_lmeval_arc_easy = "trustyai_lmeval::arc_easy"
+        provider_id = "trustyai_lmeval"
+        trustyai_lmeval_arc_easy = f"{provider_id}::arc_easy"
         llamastack_client.benchmarks.register(
             benchmark_id=trustyai_lmeval_arc_easy,
             dataset_id=trustyai_lmeval_arc_easy,
@@ -41,5 +42,8 @@ class TestLlamaStackLMEvalProvider:
             metadata={"tokenized_request": False, "tokenizer": "google/flan-t5-small"},
         )
 
-        response = llamastack_client.benchmarks.list()
-        print(response)
+        benchmarks = llamastack_client.benchmarks.list()
+
+        assert len(benchmarks) == 1
+        assert benchmarks[0].identifier == trustyai_lmeval_arc_easy
+        assert benchmarks[0].provider_id == provider_id
