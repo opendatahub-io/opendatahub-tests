8f5c7f252558c1f662f012f39c5b671e9935d86b
Milind Waykole <mwaykole@redhat.com>
2025-08-08 17:56:42 +0530

    skip kueue for 2.22 in model server
    Signed-off-by: Milind Waykole <mwaykole@redhat.com>

--- tests/model_serving/model_server/kueue/test_kueue_isvc_serverless.py
+++ tests/model_serving/model_server/kueue/test_kueue_isvc_serverless.py
@@ -68,7 +68,7 @@ EXPECTED_UPDATED_DEPLOYMENTS = 2
     ],
     indirect=True,
 )
-@pytest.mark.skip(reason="2.22 we dont support kueue")
+@pytest.mark.skip(reason="kueue is not supported in 2.22")
 class TestKueueInferenceServiceServerless:
     """Test inference service with serverless deployment"""
 
