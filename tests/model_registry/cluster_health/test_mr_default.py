21f37cc1b1027ff589609a317cfa22a5704e2d23
fege <fmosca@redhat.com>
2025-09-02 14:57:34 +0200

    change: split test and use fixture for the dsc_resource

--- tests/model_registry/cluster_health/test_mr_default.py
+++ tests/model_registry/cluster_health/test_mr_default.py
@@ -1,8 +1,7 @@
 import pytest
-from utilities.infra import get_data_science_cluster
 from utilities.constants import DscComponents
-from kubernetes.dynamic import DynamicClient
 
+from ocp_resources.data_science_cluster import DataScienceCluster
 from ocp_resources.namespace import Namespace
 
 from simple_logger.logger import get_logger
@@ -12,18 +11,17 @@ from pytest_testconfig import config as py_config
 LOGGER = get_logger(name=__name__)
 
 
+@pytest.mark.cluster_health
 class TestMrDefault:
-    @pytest.mark.cluster_health
-    def test_mr_default(self, admin_client: DynamicClient):
-        """
-        Verify MODELREGISTRY managementState is MANAGED in DSC,
-        the namespace is Active, and the MR ready condition in DSC is True.
-        """
-        dsc_resource = get_data_science_cluster(client=admin_client)
+    def test_mr_management_state(self, dsc_resource: DataScienceCluster):
+        """Verify MODELREGISTRY managementState is MANAGED in DSC."""
         assert (
             dsc_resource.instance.spec.components[DscComponents.MODELREGISTRY].managementState
             == DscComponents.ManagementState.MANAGED
         )
+
+    def test_mr_namespace_exists_and_active(self, dsc_resource: DataScienceCluster):
+        """Verify MR namespace exists and is in Active state."""
         namespace = Namespace(
             name=dsc_resource.instance.spec.components[DscComponents.MODELREGISTRY].registriesNamespace,
             ensure_exists=True,
@@ -31,10 +29,11 @@ class TestMrDefault:
         assert namespace.instance.status.phase == Namespace.Status.ACTIVE
         assert namespace.instance.metadata.name == py_config["model_registry_namespace"]
 
+    def test_mr_condition_in_dsc(self, dsc_resource: DataScienceCluster):
+        """Verify MR ready condition is True in DSC."""
         for condition in dsc_resource.instance.status.conditions:
             if condition.type == DscComponents.COMPONENT_MAPPING[DscComponents.MODELREGISTRY]:
-                LOGGER.info(f"MR ready in DSC: {condition.status}")
                 assert condition.status == "True"
                 break
         else:
-            pytest.fail("MR not ready in DSC")
+            pytest.fail("MR ready condition not found in DSC")
