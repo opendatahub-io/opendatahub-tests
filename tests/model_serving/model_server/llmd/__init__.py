41c26e8b6eb995f0881b8c6bb413322df2256af0
Milind Waykole <mwaykole@redhat.com>
2025-09-16 13:01:32 +0530

    fix precommit
    Signed-off-by: Milind Waykole <mwaykole@redhat.com>

--- tests/model_serving/model_server/llmd/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-LLM Deployment (LLMD) tests module.
-
-This module contains tests for LLM deployment functionality including:
-- Gateway resource creation and management
-- LLMInferenceService deployment and configuration
-- Integration testing between gateway and inference services
-"""
