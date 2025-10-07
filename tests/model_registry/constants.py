a9566ff2a2d4a67d00c36dc55cacf3c60af79082
Debarati Basu-Nag <dbasunag@redhat.com>
2025-10-07 09:23:41 -0400

    Add upgrade tests to 2.16 (#685)

--- tests/model_registry/constants.py
+++ tests/model_registry/constants.py
@@ -1,2 +1,18 @@
+from utilities.constants import Annotations
+
+
 class ModelRegistryEndpoints:
     REGISTERED_MODELS: str = "/api/model_registry/v1alpha3/registered_models"
+
+
+MODEL_NAME: str = "my-model"
+MODEL_DESCRIPTION: str = "lorem ipsum"
+DB_RESOURCES_NAME: str = "db-model-registry0"
+MR_INSTANCE_NAME: str = "model-registry0"
+MR_OPERATOR_NAME: str = "model-registry-operator"
+MR_NAMESPACE: str = "rhoai-model-registries"
+DEFAULT_LABEL_DICT_DB: dict[str, str] = {
+    Annotations.KubernetesIo.NAME: DB_RESOURCES_NAME,
+    Annotations.KubernetesIo.INSTANCE: DB_RESOURCES_NAME,
+    Annotations.KubernetesIo.PART_OF: DB_RESOURCES_NAME,
+}
