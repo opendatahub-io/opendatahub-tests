868f7f35ac78f966cc64e8e21feeb6ef2a5de89a
lugi0 <lgiorgi@redhat.com>
2025-10-10 14:45:14 +0200

    feat: Add upgrade scenario for model catalog in 3.0
    Signed-off-by: lugi0 <lgiorgi@redhat.com>

---
+++ tests/model_registry/model_catalog/upgrade/constants.py
@@ -0,0 +1,5 @@
+# Upgrade-specific test data
+UPGRADE_TEST_CATALOG_ID = "upgrade_test_catalog"
+UPGRADE_TEST_MODELS = ["test-model/upgrade-test-v1", "test-model/upgrade-test-v2"]
+
+TEST_DATA = {"catalog_id": UPGRADE_TEST_CATALOG_ID, "models": UPGRADE_TEST_MODELS}
