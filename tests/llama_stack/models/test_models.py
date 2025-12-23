326fb15a10c1299653e1522ed23b1cc5c6ff837f
Jorge Garcia Oncins <jgarciao@redhat.com>
2025-12-23 11:12:06 +0100

    fix: fix and enhance models tests
    Signed-off-by: Jorge Garcia Oncins <jgarciao@redhat.com>

--- tests/llama_stack/models/test_models.py
+++ tests/llama_stack/models/test_models.py
@@ -1,31 +1,21 @@
 import pytest
+import os
 
 from tests.llama_stack.constants import LlamaStackProviders
-from llama_stack_client import LlamaStackClient
-from utilities.constants import MinIo, QWEN_MODEL_NAME
+from llama_stack_client import LlamaStackClient, NotFoundError
 
 
 @pytest.mark.parametrize(
-    "model_namespace, minio_pod, minio_data_connection, llama_stack_server_config",
+    "unprivileged_model_namespace",
     [
         pytest.param(
             {"name": "test-llamastack-models", "randomize_name": True},
-            MinIo.PodConfig.QWEN_HAP_BPIV2_MINIO_CONFIG,
-            {"bucket": "llms"},
-            {
-                "vllm_url_fixture": "qwen_isvc_url",
-                "inference_model": QWEN_MODEL_NAME,
-                "llama_stack_storage_size": "2Gi",
-            },
-        )
+        ),
     ],
     indirect=True,
 )
-@pytest.mark.rawdeployment
-@pytest.mark.smoke
 @pytest.mark.llama_stack
-@pytest.mark.skip_must_gather
-@pytest.mark.usefixtures("minio_pod", "minio_data_connection")
+@pytest.mark.smoke
 class TestLlamaStackModels:
     """Test class for LlamaStack models API functionality.
 
@@ -34,9 +24,13 @@ class TestLlamaStackModels:
     - https://github.com/openai/openai-python/blob/main/api.md#models
     """
 
-    def test_models_list(self, llama_stack_client: LlamaStackClient) -> None:
+    @pytest.mark.smoke
+    def test_models_list(
+        self,
+        unprivileged_llama_stack_client: LlamaStackClient,
+    ) -> None:
         """Test the initial state of the LlamaStack server and available models."""
-        models = llama_stack_client.models.list()
+        models = unprivileged_llama_stack_client.models.list()
         assert models is not None, "No models returned from LlamaStackClient"
 
         llm_model = next((model for model in models if model.api_model_type == "llm"), None)
@@ -52,26 +46,34 @@ class TestLlamaStackModels:
         embedding_dimension = embedding_model.metadata["embedding_dimension"]
         assert embedding_dimension is not None, "No embedding_dimension set in embedding model"
 
-    def test_models_register(self, llama_stack_client: LlamaStackClient) -> None:
+    def test_models_register_retrieve_unregister(self, unprivileged_llama_stack_client: LlamaStackClient) -> None:
         """Test model registration functionality."""
-        response = llama_stack_client.models.register(
-            provider_id=LlamaStackProviders.Inference.VLLM_INFERENCE, model_type="llm", model_id=QWEN_MODEL_NAME
+
+        # Test model registration
+        inference_model = os.getenv("LLS_CORE_INFERENCE_MODEL", "")
+        test_model_id = f"{inference_model}-2"
+
+        response = unprivileged_llama_stack_client.models.register(
+            model_id=test_model_id,
+            model_type="llm",
+            provider_id=LlamaStackProviders.Inference.VLLM_INFERENCE,
         )
         assert response
 
-    def test_model_list(self, llama_stack_client: LlamaStackClient) -> None:
-        """Test listing available models and verify their properties."""
-        models = llama_stack_client.models.list()
+        models = unprivileged_llama_stack_client.models.list()
+        print(models)
 
-        # Find the registered LLM by identifier suffix
-        expected_id_suffix = f"/{QWEN_MODEL_NAME}"
-        target = next(
-            (model for model in models if model.model_type == "llm" and model.identifier.endswith(expected_id_suffix)),
-            None,
-        )
-        assert target is not None, (
-            f"LLM {QWEN_MODEL_NAME} not found in models: {[model.identifier for model in models]}"
-        )
-        assert target.identifier.endswith(expected_id_suffix)
-        assert target.model_type == "llm"
-        assert target.provider_id == LlamaStackProviders.Inference.VLLM_INFERENCE.value
+        # Test model retrieval
+        registered_model_id = f"{LlamaStackProviders.Inference.VLLM_INFERENCE.value}/{inference_model}-2"
+        registered_model = unprivileged_llama_stack_client.models.retrieve(model_id=registered_model_id)
+        assert registered_model is not None, f"LLM {registered_model_id} not found using models.retrieve"
+        expected_id_suffix = f"/{test_model_id}"
+        assert registered_model.identifier.endswith(expected_id_suffix)
+        assert registered_model.api_model_type == "llm"
+        assert registered_model.provider_id == LlamaStackProviders.Inference.VLLM_INFERENCE.value
+
+        # Test model deletion
+        unprivileged_llama_stack_client.models.unregister(model_id=registered_model_id)
+
+        with pytest.raises(NotFoundError):
+            unprivileged_llama_stack_client.models.retrieve(model_id=registered_model_id)
