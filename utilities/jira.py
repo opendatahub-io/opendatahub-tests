662a01ccd8fb20e2a36dd399b8d4be8162eda2a1
Debarati Basu-Nag <dbasunag@redhat.com>
2026-03-19 16:27:28 -0400

    fix: undo the changes in conftest.py and jira.py
    Signed-off-by: Debarati Basu-Nag <dbasunag@redhat.com>

--- utilities/jira.py
+++ utilities/jira.py
@@ -24,7 +24,7 @@ def get_jira_connection() -> JIRA:
     """
     return JIRA(
         server=os.getenv("PYTEST_JIRA_URL"),
-        basic_auth=(os.getenv("PYTEST_JIRA_USERNAME"), os.getenv("PYTEST_JIRA_PASSWORD")),
+        basic_auth=(os.getenv("PYTEST_JIRA_USERNAME"), os.getenv("PYTEST_JIRA_TOKEN")),
     )
 
 
