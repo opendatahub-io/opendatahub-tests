31bb3d19d4a6792ca86a6d45f1fd2138382b52cd
Milind Waykole <mwaykole@redhat.com>
2026-03-23 17:51:25 +0530

    changes to fix model server Test (#1271)
    * changes to fix model server Test
    
    Signed-off-by: Milind waykole <mwaykole@redhat.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Milind waykole <mwaykole@redhat.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/model_serving/model_server/upgrade/test_upgrade.py
+++ tests/model_serving/model_server/upgrade/test_upgrade.py
@@ -1,9 +1,8 @@
 import pytest
 
 from tests.model_serving.model_server.utils import verify_inference_response
-from utilities.constants import ModelName, Protocols
+from utilities.constants import Protocols
 from utilities.inference_utils import Inference
-from utilities.manifests.caikit_standalone import CAIKIT_STANDALONE_INFERENCE_CONFIG
 from utilities.manifests.onnx import ONNX_INFERENCE_CONFIG
 from utilities.manifests.openvino import OPENVINO_INFERENCE_CONFIG
 
@@ -27,14 +26,13 @@ class TestPreUpgradeModelServer:
         )
 
     @pytest.mark.pre_upgrade
-    def test_raw_caikit_bge_pre_upgrade_inference(self, caikit_raw_inference_service_scope_session):
-        """Test Caikit bge-large-en embedding model inference using internal route before upgrade"""
+    def test_raw_ovms_onnx_pre_upgrade_inference(self, ovms_raw_inference_service_scope_session):
+        """Verify that kserve Raw OVMS ONNX model can be queried using REST before upgrade"""
         verify_inference_response(
-            inference_service=caikit_raw_inference_service_scope_session,
-            inference_config=CAIKIT_STANDALONE_INFERENCE_CONFIG,
-            inference_type="embedding",
-            protocol=Protocols.HTTPS,
-            model_name=ModelName.CAIKIT_BGE_LARGE_EN,
+            inference_service=ovms_raw_inference_service_scope_session,
+            inference_config=ONNX_INFERENCE_CONFIG,
+            inference_type=Inference.INFER,
+            protocol=Protocols.HTTP,
             use_default_query=True,
         )
 
@@ -73,21 +71,20 @@ class TestPostUpgradeModelServer:
         )
 
     @pytest.mark.post_upgrade
-    @pytest.mark.dependency(name="test_raw_caikit_bge_post_upgrade_inference_exists")
-    def test_raw_caikit_bge_post_upgrade_inference_exists(self, caikit_raw_inference_service_scope_session):
-        """Test that raw deployment inference service exists after upgrade"""
-        assert caikit_raw_inference_service_scope_session.exists
+    @pytest.mark.dependency(name="test_raw_ovms_onnx_post_upgrade_inference_exists")
+    def test_raw_ovms_onnx_post_upgrade_inference_exists(self, ovms_raw_inference_service_scope_session):
+        """Test that raw OVMS inference service exists after upgrade"""
+        assert ovms_raw_inference_service_scope_session.exists
 
     @pytest.mark.post_upgrade
-    @pytest.mark.dependency(depends=["test_raw_caikit_bge_post_upgrade_inference_exists"])
-    def test_raw_caikit_bge_post_upgrade_inference(self, caikit_raw_inference_service_scope_session):
-        """Test Caikit bge-large-en embedding model inference using internal route after upgrade"""
+    @pytest.mark.dependency(depends=["test_raw_ovms_onnx_post_upgrade_inference_exists"])
+    def test_raw_ovms_onnx_post_upgrade_inference(self, ovms_raw_inference_service_scope_session):
+        """Verify that kserve Raw OVMS ONNX model can be queried using REST after upgrade"""
         verify_inference_response(
-            inference_service=caikit_raw_inference_service_scope_session,
-            inference_config=CAIKIT_STANDALONE_INFERENCE_CONFIG,
-            inference_type="embedding",
-            protocol=Protocols.HTTPS,
-            model_name=ModelName.CAIKIT_BGE_LARGE_EN,
+            inference_service=ovms_raw_inference_service_scope_session,
+            inference_config=ONNX_INFERENCE_CONFIG,
+            inference_type=Inference.INFER,
+            protocol=Protocols.HTTP,
             use_default_query=True,
         )
 
