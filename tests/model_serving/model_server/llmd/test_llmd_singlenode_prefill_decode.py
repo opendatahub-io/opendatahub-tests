344aa4d17a54e763518c7b949bcc5bccda409a74
threcc <trecchiu@redhat.com>
2026-06-11 11:23:38 +0200

    fix expected scheduler plugin
    Signed-off-by: threcc <trecchiu@redhat.com>

--- tests/model_serving/model_server/llmd/test_llmd_singlenode_prefill_decode.py
+++ tests/model_serving/model_server/llmd/test_llmd_singlenode_prefill_decode.py
@@ -103,7 +103,8 @@ class TestSingleNodePrefillDecode:
 
         # Verify scheduler config contains all expected P/D plugins
         for expected_plugin in [
-            "disagg-headers-handler",
+            # "disagg-headers-handler", in 3.5ea1 the expect plugin is "prefill-header-handler"
+            "prefill-header-handler"
             "prefill-filter",
             "decode-filter",
             "always-disagg-pd-decider",
