c59c59b6ac936bbda8e6d2a254ce93406e32419a
Sindhujabd <sindhujabd@ibm.com>
2026-07-14 19:37:55 +0200

    Use immutable digests for s390x MinIO images
    Signed-off-by: Sindhujabd <sindhujabd@ibm.com>

--- tests/ai_safety/constants.py
+++ tests/ai_safety/constants.py
@@ -1,5 +1,5 @@
 MINIO_IMAGE = "quay.io/minio/minio@sha256:46b3009bf7041eefbd90bd0d2b38c6ddc24d20a35d609551a1802c558c1c958f"
 MINIO_MC_IMAGE = "quay.io/minio/mc@sha256:470f5546b596e16c7816b9c3fa7a78ce4076bb73c2c73f7faeec0c8043923123"
 
-MINIO_IMAGE_S390X = "quay.io/trustyai_testing/minio:latest"
-MINIO_MC_IMAGE_S390X = "quay.io/trustyai_testing/minio-mc:latest"
+MINIO_IMAGE_S390X = "quay.io/trustyai_testing/minio@sha256:abc50e009bc2c16a72ae2dedf6f8445da73f261d6ca7b8a937fc354b37396c87"
+MINIO_MC_IMAGE_S390X = "quay.io/trustyai_testing/minio-mc@sha256:92047fda5f59166ecdf6d7ed4f3f09cd09e0fd698803fb549e936fc1903e0b5b"
