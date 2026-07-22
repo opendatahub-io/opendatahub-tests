b1be6eec5547d3765f2791812df8fcb7d09f6376
NisargaKN <nisargk1@in.ibm.com>
2026-07-22 05:28:37 +0000

    Add tokenizer cache directory support for llmd-inference-sim for s390x
    Signed-off-by: NisargaKN <nisargk1@in.ibm.com>

--- tests/fixtures/inference.py
+++ tests/fixtures/inference.py
@@ -100,8 +100,16 @@ def llm_d_inference_sim_serving_runtime(
                         str(LLMdInferenceSimConfig.port),
                         "--max-model-len",
                         str(LLMdInferenceSimConfig.max_model_len),
+                         "--tokenizers-cache-dir",
+                         "/data/tokenizers_cache",
                     ],
                     "ports": [{"containerPort": LLMdInferenceSimConfig.port, "protocol": "TCP"}],
+                     "volumeMounts": [
+                    {
+                        "name": "tokenizers-cache",
+                        "mountPath": "/data/tokenizers_cache",
+                    }
+                    ],
                     "securityContext": {
                         "allowPrivilegeEscalation": False,
                     },
@@ -120,6 +128,12 @@ def llm_d_inference_sim_serving_runtime(
                         "timeoutSeconds": 5,
                     },
                 }
+            ],
+            volumes=[
+            {
+                "name": "tokenizers-cache",
+                "emptyDir": {},
+            }
             ],
             multi_model=False,
             supported_model_formats=[{"autoSelect": True, "name": LLMdInferenceSimConfig.name}],
