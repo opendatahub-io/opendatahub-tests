e9e850528e227e8013577c44a58d8abbcd27161e
Nishidha Panpaliya <nishidha.panpaliya@partner.ibm.com>
2026-08-25 16:29:55 +0530

    Power specific changes to build opendatahub-test image multiarch
    Signed-off-by: Nishidha Panpaliya <nishidha.panpaliya@partner.ibm.com>

--- tests/ogx/vector_io/conftest.py
+++ tests/ogx/vector_io/conftest.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from collections.abc import Generator
 from typing import Any
 
@@ -7,6 +8,13 @@ import pytest
 import structlog
 from ogx_client import OgxClient
 from ogx_client.types.vector_store import VectorStore
+
+if platform.machine() == "ppc64le":
+    pytest.skip(
+        "ragas is not supported on PPC64LE",
+        allow_module_level=True,
+    )
+
 from ragas import SingleTurnSample
 
 from tests.ogx.constants import (
